143e2646a93c36693e3cdd2b83a98e3804753d34
OgheneDev <emmanueloghene72@gmail.com>
2024-12-10 20:40:38 +0100

    Scroll

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, {useEffect} from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../components/Layout/Header'
 import Navbar from '../components/Layout/Navbar'
 import Footer from '../components/Layout/Footer'
 
 const MainLayout = () => {
+  const location = useLocation(); // Get the current location
+
+  useEffect(() => {
+    // Scroll to top when the path changes
+    window.scrollTo(0, 0);
+  }, [location.pathname]); // Run this effect whenever the route changes
+  
   return (
     <>
       <Header />
